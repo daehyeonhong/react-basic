a4622bc092f1731e7cfc980b93050e7f5250ed49
DaeHyeonHong <fholng@outlook.com>
2022-10-24 23:27:02 +0900

    jsx 심화문법

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import {Fragment} from 'react';
 
 function App() {
     const name = '뿡';
-    return (
-        <Fragment>
-            <h1 className={'orange'}>Hello, World!</h1>
-            <h2>Hello, World!</h2>
-            <p>{name}</p>
-            <ul>
-                <li>우유</li>
-                <li>딸기</li>
-                <li>바나나</li>
-            </ul>
-            <img
-                style={{width: '200px', height: '200px'}}
-                src={'https://images.unsplash.com/photo-1666521806890-2216eaa46a71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'}
-                alt={''}/>
-        </Fragment>
-    );
+    const list = ['우유', '딸기', '바나나'];
+    return (<Fragment>
+        <h1 className={'orange'}>{`Hello, ${name}!`}</h1>
+        <h2>Hello, World!</h2>
+        <p>{name}</p>
+        <ul>{list.map(item => <li>{item}</li>)}
+        </ul>
+        <img
+            style={{width: '200px', height: '200px'}}
+            src={'https://images.unsplash.com/photo-1666521806890-2216eaa46a71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'}
+            alt={'empty'}/>
+    </Fragment>);
 }
 
 export default App;
