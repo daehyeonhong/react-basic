5def53a6cb34f4c6604489bf691931f947a58bd0
DaeHyeonHong <fholng@outlook.com>
2022-10-24 23:14:56 +0900

    className:

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Fragment} from 'react';
 function App() {
     return (
         <Fragment>
-            <h1>Hello, World!</h1>
+            <h1 className={`orange`}>Hello, World!</h1>
             <h2>Hello, World!</h2>
         </Fragment>
     );
